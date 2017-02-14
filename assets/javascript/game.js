4e912231f80d311eadb096024944e9d118727f39
maade7 <martin.ade@gatech.edu>
2017-02-14 01:27:25 -0500

    CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,16 @@
-var names = ['James T. Kirk', 'Spock', 'Leonard McCoy', 'Pavel Chekov', 'Montgomery Scott', 'Nyota Uhura', 'Hikaru Sulu', 'Jean-Luc Picard', 'William T. Riker', 'Data', 'Geordi La Forge', 'Worf', 'Deanna Troi', 'Natasha Yar', 'Odo', 'Benjamin Sisko', 'Julian Bashir', 'Jadzia Dax', 'Kira Nerys', 'Kathryn Janeway', 'Chakotay', 'Tuvok', 'Seven of Nine', 'Miles O\'Brien']
+var names = ['James T. Kirk', 'Spock', 'Leonard McCoy', 'Montgomery Scott', 'Uhura', 'Hikaru Sulu', 'Pavel Chekov', 'Christine Chapel', 'Janice Rand', 'Jean-Luc Picard', 'William T. Riker', 'Deanna Troi', 'Data', 'Geordi La Forge', 'Worf', 'Beverly Crusher', 'Wesley Crusher', 'Tasha Yar', 'Benjamin Sisko', 'Kira Nerys', 'Odo', 'Jadzia Dax', 'Ezri Dax', 'Julian Bashir', 'Miles O\'Brien', 'Quark', 'Kathryn Janeway', 'Chakotay', 'Tuvok', 'B\'Elanna Torres', 'Tom Paris', 'Harry Kim', 'The Doctor', 'Neelix', 'Kes', 'Seven of Nine', 'Jonathan Archer', 'T\'Pol', 'Trip Tucker', 'Phlox', 'Hoshi Sato', 'Malcolm Reed', 'Travis Mayweather', 'Porthos'];
 var person = [];
 var pick = '';
 var displayName = document.getElementById("name");
 var displayTries = document.getElementById("tries");
 var displayScore = document.getElementById("score");
 var displayAttempted = document.getElementById("attempted");
-var displayBio = document.getElementById("bio");
+var displayAccess = document.getElementById("access");
+var obj = document.getElementById('characters');
 var tries = 10;
 var score = 0;
 var attempted = '';
+var access = 'ACCESS DENIED';
 
 
 
@@ -32,7 +34,6 @@ function reset() {
 //user guesses
 document.onkeyup = function(event) {
     if (pick.toLowerCase().indexOf(event.key.toLowerCase()) === -1 || attempted.toLowerCase().indexOf(event.key.toLowerCase()) !== -1) {
-        console.log(pick.indexOf(event.key));
         //wrong key
         tries = tries - 1;
     } else {
@@ -48,25 +49,30 @@ document.onkeyup = function(event) {
     wright();
 }
 
-function personBio(bio) {
-    displayBio.innerHTML = bio;
+function move(y) {
+    obj.style.position = 'relative';
+    obj.style.top = ((y * -50) + 'rem');
+    console.log((y * -50) + 'rem');
 }
+
 
 function wright() {
     if (tries === 0) {
         alert('Attempts Exhausted: Access Denied' + String.fromCharCode(10) + 'Score:  ' + score);
         score = 0;
+        access = 'ACCESS DENIED';
         reset();
     }
     if (person.indexOf('*') === -1) {
-        alert('Access Granted' + String.fromCharCode(10) + 'Score:  ' + score);
-        personBio(pick);
+        access = 'ACCESS GRANTED';
+        move(names.indexOf(pick));
         reset();
     }
     displayName.innerText = person.join('');
     displayTries.innerHTML = tries;
     displayScore.innerHTML = score;
     displayAttempted.innerHTML = attempted;
+    displayAccess.innerHTML = access;
 }
 
 console.log(names);
